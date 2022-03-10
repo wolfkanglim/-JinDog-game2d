148e8659091a2e0024ad1796ab7fba9886f3d253
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-09 21:54:28 -0500

    added roll, import from input

--- player.js
+++ player.js
@@ -1,21 +1,24 @@
-import {StandingRight, StandingLeft,SittingRight, SittingLeft,RunningRight, RunningLeft, JumpingRight, JumpingLeft,FallLeft, FallRight} from './state.js';
+import {StandingRight, StandingLeft,SittingRight, SittingLeft,RunningRight, RunningLeft, JumpingRight, JumpingLeft,FallLeft, FallRight, RollRight, RollLeft} from './state.js';
+
+ const dogImage = document.getElementById('dog_sprite');
 
 export default class Player{
     constructor(gameWidth, gameHeight){        
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;        
-        this.image = document.getElementById('dog_sprite');
-        this.states = [new StandingRight(this), new StandingLeft(this),new SittingRight(this), new SittingLeft(this), new RunningRight(this), new RunningLeft(this), new JumpingRight(this), new JumpingLeft(this), new FallRight(this), new FallLeft(this)]; 
+        this.image = dogImage;
+        this.states = [new StandingRight(this), new StandingLeft(this), new SittingRight(this), new SittingLeft(this), new RunningRight(this), new RunningLeft(this), new JumpingRight(this), new JumpingLeft(this), new FallRight(this), new FallLeft(this), new RollRight(this), new RollLeft(this)]; 
         this.currentState = this.states[0];
         this.width = 200;
         this.height = 181.83;
         this.frameX = 0;
         this.frameY = 0;
         this.x = this.gameWidth / 2 - this.width / 2;
-        this.y = this.gameHeight - this.height - 260;
+        this.y = this.gameHeight - this.height - 250;
         this.speed = 0;
         this.maxSpeed = 10;
         this.vy = 0;
+        this.life = 3;
         this.gravity = 1;
         //deltaTime frame animation
         this.maxFrame = 4;
@@ -25,7 +28,7 @@ export default class Player{
     }
     restart(){
         this.x = 200;
-        this.y = this.gameHeight - this.height / 2 - 320;
+        this.y = this.gameHeight - this.height - 250;
     }
     draw(context, deltaTime){
         if(this.frameTimer > this.frameInterval){
@@ -44,8 +47,8 @@ export default class Player{
     update(input){      
         this.currentState.handleInput(input);       
         this.x += this.speed;
-        if(this.x < 0) this.x = 0;
-        if(this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+        if(this.x < this.width / 4) this.x = this.width / 4;
+        if(this.x > this.gameWidth - this.width /4) this.x = this.gameWidth - this.width / 4;
       
         //vertical movement
         this.y += this.vy;
@@ -67,17 +70,17 @@ export default class Player{
 //player trails
 
 export class PlayerTrails{
-    constructor(gameWidth, gameHeight, x, y){        
+    constructor(gameWidth, gameHeight, x, y, color, size){        
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
         this.x = x;
         this.y = y;
-        this.size = Math.random() * 8 + 1;
+        this.size = size * Math.random() * 8;
         this.speedY = Math.random() * 0.4 - 0.3;
-        //this.color = '#fff';
+        this.color = color;
     }
     draw(context){
-        context.fillStyle = 'white';
+        context.fillStyle = this.color;
         context.beginPath();
         context.arc(this.x + 5, this.y + 18, this.size, 0 , Math.PI * 2);
         context.fill();
@@ -88,3 +91,6 @@ export class PlayerTrails{
     }
 }
 
+const dogStillCutImage = function(){
+    context.drawImage(dogImage, 0,0, 200, 181.83, 100, 100, 30,30);
+}
