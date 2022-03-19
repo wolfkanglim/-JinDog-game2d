0bf9eeefb6698ce11723ce9f171bd9981a548ca3
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-19 09:06:13 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
  export function drawStatusText(context, input, player){
     context.fillStyle = 'white';
     context.font = '18px Arial';
-      context.fillText('Last Input: ' + input.lastkey, 30, 100);
-    context.fillText('Active Status: ' + player.currentState.state, 30, 120);
+      //context.fillText('Last Input: ' + input.lastkey, 30, 100);
+    //context.fillText('Active Status: ' + player.currentState.state, 30, 120);
     context.fillText('Jin Dog: ' + player.life, 130, 100);
    
 }
