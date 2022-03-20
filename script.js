34d72f1f20eb333f0527451c8ae4f1301d38e3b7
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-20 04:07:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -327,6 +327,7 @@ export function init(){
                 bgm.play();
                 gameOver = false;              
                 animate(0);
+               }
         })
                   
                  
