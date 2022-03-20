bf682a4589f782c0d6c7f9f81be0adafcf11e59f
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-19 23:36:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ export function init(){
   
        ///canvas pause and how to play
          const paused = document.getElementById('paused');
-        canvas.addEventListener('click', () => {
+        canvas.addEventListener('touchstart', () => {
             if(!gameOver){
                 bgm.pause();                    
                 gameOver = true;
