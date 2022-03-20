c4d7fa58d8d0f440c17e23d8bbf4df9116b3a011
truckDriverDev <hungrywolfkang@gmail.com>
2022-03-20 02:40:09 -0400

    back to mousedown

--- input.js
+++ input.js
@@ -3,7 +3,7 @@
 //     pantingSound.volume = 0.5;
   const barkTwiceSound = new Audio();
     barkTwiceSound.src = 'sounds/sounds_dog-barking-twice.wav';    
-    barkTwiceSound.volume = 0.5;
+    barkTwiceSound.volume = 0.2;
     const fireBallSound = new Audio();
     fireBallSound.src = 'sounds/sounds_fireball3.mp3';
     fireBallSound.volume = 0.2;
@@ -11,8 +11,6 @@
 export default class handleInput{
     constructor(){
         this.lastkey = '';
-        this.touchY = 0;
-        this.touchTreshold = 30;
         window.addEventListener('keydown', (e) => {
             if((e.ctrlKey) && e.key === 'ArrowRight'){
                 this.lastkey = 'PRESS ctrl right';
@@ -66,17 +64,7 @@ export default class handleInput{
 //             pantingSound.currentTime = 0;                       
 //                      pantingSound.play();    
         }) 
-        
-        /////mouse toggle for Pause Game///// 
-//         window.addEventListener('mouseclick', (e) => {
-//             console.log('pause');
-//             console.log(e);
-//             if(!gameOver){
-//                 gameOver = true;
-//             } else {
-//                 gameOver = false;
-//             }
-//         })        
+      
 
       /////control button input/////
         const jumpLeft = document.getElementById('jumpLeft');
@@ -86,58 +74,72 @@ export default class handleInput{
         const rollLeft = document.getElementById('rollLeft');
         const rollRight = document.getElementById('rollRight');
 
-        jumpLeft.addEventListener('touchstart', (e) => {
-            //e.preventDefault();
+         jumpLeft.addEventListener('mousedown', () => {
             this.lastkey = "PRESS up";
-             barkTwiceSound.currentTime = 0;
-                     barkTwiceSound.play();
+            barkTwiceSound.currentTime = 0;
+            barkTwiceSound.play();
         })
-        jumpLeft.addEventListener('touchend', () => {
+        jumpLeft.addEventListener('mouseup', () => {
             this.lastkey = 'RELEASE up';
         })
-        jumpRight.addEventListener('touchstart', (e) => {
-          //e.preventDefault();
+        jumpRight.addEventListener('mousedown', () => {
             this.lastkey = "PRESS up";
-             barkTwiceSound.currentTime = 0;
-                     barkTwiceSound.play();
+            barkTwiceSound.currentTime = 0;
+            barkTwiceSound.play();
         })
-        jumpRight.addEventListener('touchend', () => {
+        jumpRight.addEventListener('mouseup', () => {
             this.lastkey = 'RELEASE up';
         })
 
-        runLeft.addEventListener('touchstart', (e) => {
-          //e.preventDefault();
+        runLeft.addEventListener('mousedown', () => {
             this.lastkey = "PRESS left";
         })
-        runLeft.addEventListener('touchend', () => {
+        runLeft.addEventListener('mouseup', () => {
             this.lastkey = 'RELEASE left';
+            pantingSound.play();  
         })
 
-        runRight.addEventListener('touchstart', (e) => {
-          //e.preventDefault();
+        runRight.addEventListener('mousedown', () => {
             this.lastkey = "PRESS right";
         })
-        runRight.addEventListener('touchend', () => {
+        runRight.addEventListener('mouseup', () => {
+            this.lastkey = 'RELEASE right';
+            pantingSound.play();  
+        })
+
+        rollLeft.addEventListener('mousedown', () => {
+            this.lastkey = 'PRESS ctrl left';
+            fireBallSound.play();
+            fireBallSound.currentTime = 0;
+        })
+        rollLeft.addEventListener('mouseup', () => {
+            this.lastkey = 'RELEASE left';
+        })
+        rollRight.addEventListener('mousedown', () => {
+            this.lastkey = 'PRESS ctrl right';
+            fireBallSound.play();
+            fireBallSound.currentTime = 0;
+        })
+        rollRight.addEventListener('mouseup', () => {
             this.lastkey = 'RELEASE right';
         })
 
-        rollLeft.addEventListener('touchstart', (e) => {
-          //e.preventDefault();
-            this.lastkey = 'PRESS ctrl left';
-             fireBallSound.currentTime = 0;
-                fireBallSound.play();
+        ///// touchsreen ///// 
+        ///// need to add 'swipe up' 'swipe down' in inputHandler at state.js 
+
+      /*    window.addEventListener('touchstart', (e) => {
+            this.touchY = e.changedTouches[0].pageY;
         })
-        rollLeft.addEventListener('touchend', () => {
-            this.lastkey = 'RELEASE left';
+        window.addEventListener('touchmove', (e) => {
+            console.log('move');
+            let swipeDistance = e.changedTouches[0].pageY - this.touchY;
+            if(swipeDistance < - this.touchTreshold && this.lastkey.indexOf('swipe up') === -1) this.lastkey.push('swipe up');
+            else if(swipeDistance > this.touchTreshold && this.lastkey.indexOf('swipe down') === -1) this.lastkey.push('swipe down');
         })
-        rollRight.addEventListener('touchstart', (e) => {
-          //e.preventDefault();
-            this.lastkey = 'PRESS ctrl right';
-             fireBallSound.currentTime = 0;
-                fireBallSound.play();
-        })
-        rollRight.addEventListener('touchend', () => {
-            this.lastkey = 'RELEASE right';
-        }) 
-     }
+       window.addEventListener('touchend', (e) => {
+            console.log('end');
+            this.lastkey.splice(this.lastkey.indexOf('swipe up'), 1);
+            this.lastkey.splice(this.lastkey.indexOf('swipe down'), 1);
+        }) */
+    }
 }
